51fe3e9a2d2055c8e9bca06f6d4271b3d821ea3e
xlzy <1017866168@qq.com>
2023-03-25 17:41:16 +0800

    【更新】调整XnFormContainer

--- snowy-admin-web/src/components/XnFormContainer/index.vue
+++ snowy-admin-web/src/components/XnFormContainer/index.vue
@@ -1,20 +1,17 @@
 <template>
-	<Modal v-if="isModal" :visible="visible" @cancel="cancel" v-bind="$attrs">
+	<a-modal v-if="isModal" :visible="visible" @cancel="cancel" v-bind="$attrs">
 		<template v-for="slotKey in slotKeys" #[slotKey]>
 			<slot :name="slotKey" />
 		</template>
-	</Modal>
-	<Drawer v-else :visible="visible" v-bind="$attrs" :footer-style="{ textAlign: 'right' }">
+	</a-modal>
+	<a-drawer v-else :visible="visible" v-bind="$attrs" :footer-style="{ textAlign: 'right' }">
 		<template v-for="slotKey in slotKeys" #[slotKey]>
 			<slot :name="slotKey" />
 		</template>
-	</Drawer>
+	</a-drawer>
 </template>
 
 <script>
-	import { Drawer, Modal } from 'ant-design-vue'
-	import { modalProps } from 'ant-design-vue/es/modal/Modal'
-	import { drawerProps } from 'ant-design-vue/es/drawer'
 	import { mapState } from 'pinia'
 	import { globalStore } from '@/store'
 
@@ -24,19 +21,13 @@
 	}
 	export default {
 		name: 'XnFormContainer',
-		components: {
-			Drawer,
-			Modal
-		},
 		inheritAttrs: false,
 		props: {
 			visible: {
 				type: Boolean,
 				default: false,
 				required: true
-			},
-			...modalProps,
-			...drawerProps
+			}
 		},
 		computed: {
 			...mapState(globalStore, ['formStyle']),
