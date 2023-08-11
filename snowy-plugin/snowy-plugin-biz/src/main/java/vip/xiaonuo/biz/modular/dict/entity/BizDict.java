34d2fe31536f9e10a48badbe1367aa92c4acab9e
xiaonuobase <1253070437@qq.com>
2023-08-11 19:47:31 +0800

    【更新】取消字典补丁的冗余代码

--- snowy-plugin/snowy-plugin-biz/src/main/java/vip/xiaonuo/biz/modular/dict/entity/BizDict.java
+++ snowy-plugin/snowy-plugin-biz/src/main/java/vip/xiaonuo/biz/modular/dict/entity/BizDict.java
@@ -35,32 +35,28 @@ public class BizDict extends CommonEntity {
     @ApiModelProperty(value = "id", position = 1)
     private String id;
 
-    /** 租户id */
-    @ApiModelProperty(value = "租户id", position = 2)
-    private String tenantId;
-
     /** 父id */
-    @ApiModelProperty(value = "父id", position = 3)
+    @ApiModelProperty(value = "父id", position = 2)
     private String parentId;
 
     /** 字典文字 */
-    @ApiModelProperty(value = "字典文字", position = 4)
+    @ApiModelProperty(value = "字典文字", position = 3)
     private String dictLabel;
 
     /** 字典值 */
-    @ApiModelProperty(value = "字典值", position = 5)
+    @ApiModelProperty(value = "字典值", position = 4)
     private String dictValue;
 
     /** 分类 */
-    @ApiModelProperty(value = "分类", position = 6)
+    @ApiModelProperty(value = "分类", position = 5)
     private String category;
 
     /** 排序码 */
-    @ApiModelProperty(value = "排序码", position = 7)
+    @ApiModelProperty(value = "排序码", position = 6)
     private Integer sortCode;
 
     /** 扩展信息 */
-    @ApiModelProperty(value = "扩展信息", position = 8)
+    @ApiModelProperty(value = "扩展信息", position = 7)
     @TableField(insertStrategy = FieldStrategy.IGNORED, updateStrategy = FieldStrategy.IGNORED)
     private String extJson;
 }
