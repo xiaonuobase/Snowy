9cd56c69cf2c3b10a613686e6bea636b7a0ea69e
徐玉祥 <xuyuxiang29@foxmail.com>
2024-05-10 00:47:02 +0800

    【更新】druid监控页面去除广告，操作日志柱状图只查询必要字段，否则会很慢

--- snowy-web-app/src/main/java/vip/xiaonuo/core/config/DruidConfigure.java
+++ snowy-web-app/src/main/java/vip/xiaonuo/core/config/DruidConfigure.java
@@ -12,7 +12,12 @@
  */
 package vip.xiaonuo.core.config;
 
+import com.alibaba.druid.spring.boot3.autoconfigure.properties.DruidStatProperties;
+import com.alibaba.druid.util.Utils;
+import jakarta.servlet.Filter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -22,16 +27,15 @@ import org.springframework.context.annotation.Configuration;
  * @date 2023/06/30
  **/
 @Configuration
-@ConditionalOnProperty(name = "spring.datasource.druid.web-stat-filter.enabled", havingValue = "true")
+@ConditionalOnProperty(name = "spring.datasource.druid.stat-view-servlet.enabled", havingValue = "true")
 public class DruidConfigure {
 
     /**
      * 去除druid监控页面广告
      */
-    /*@SuppressWarnings({ "rawtypes", "unchecked" })
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     @Bean
-    public FilterRegistrationBean removeDruidAdFilterRegistrationBean(DruidStatProperties properties)
-    {
+    public FilterRegistrationBean removeDruidAdFilterRegistrationBean(DruidStatProperties properties) {
         // 获取web监控页面的参数
         DruidStatProperties.StatViewServlet config = properties.getStatViewServlet();
         // 提取common.js的配置路径
@@ -39,34 +43,20 @@ public class DruidConfigure {
         String commonJsPattern = pattern.replaceAll("\\*", "js/common.js");
         final String filePath = "support/http/resources/js/common.js";
         // 创建filter进行过滤
-        Filter filter = new Filter()
-        {
-            @Override
-            public void init(FilterConfig filterConfig) throws ServletException
-            {
-            }
-            @Override
-            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-                    throws IOException, ServletException
-            {
-                chain.doFilter(request, response);
-                // 重置缓冲区，响应头不会被重置
-                response.resetBuffer();
-                // 获取common.js
-                String text = Utils.readFromResource(filePath);
-                // 正则替换banner, 除去底部的广告信息
-                text = text.replaceAll("<a.*?banner\"></a><br/>", "");
-                text = text.replaceAll("powered.*?shrek.wang</a>", "");
-                response.getWriter().write(text);
-            }
-            @Override
-            public void destroy()
-            {
-            }
+        Filter filter = (request, response, chain) -> {
+            chain.doFilter(request, response);
+            // 重置缓冲区，响应头不会被重置
+            response.resetBuffer();
+            // 获取common.js
+            String text = Utils.readFromResource(filePath);
+            // 正则替换banner, 除去底部的广告信息
+            text = text.replaceAll("<a.*?banner\"></a><br/>", "");
+            text = text.replaceAll("powered.*?shrek.wang</a>", "");
+            response.getWriter().write(text);
         };
         FilterRegistrationBean registrationBean = new FilterRegistrationBean();
         registrationBean.setFilter(filter);
         registrationBean.addUrlPatterns(commonJsPattern);
         return registrationBean;
-    }*/
+    }
 }
