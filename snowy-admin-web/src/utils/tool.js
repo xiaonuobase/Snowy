578bd2bb4caa4bed5074225f676708005706fae6
13066656961 <1622813142@qq.com>
2024-07-25 20:18:39 +0800

    时间戳格式化添加
    (cherry picked from commit a6e6f161ab9745e48f5bc6532d6a0af1c706f189)

--- snowy-admin-web/src/utils/tool.js
+++ snowy-admin-web/src/utils/tool.js
@@ -166,4 +166,43 @@ tool.snowyUuid = () => {
 	return 'xn' + uuid.slice(2)
 }
 
+tool.parseTime = (time, cFormat) => {
+	if (time == null || time.length === 0) {
+	  return ''
+	}
+
+	const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
+	let date
+	if (typeof time === 'object') {
+	  date = time
+	} else {
+	  if ((typeof time === 'string') && (/^[0-9]+$/.test(time))) {
+		time = parseInt(time)
+	  }
+	  if ((typeof time === 'number') && (time.toString().length === 10)) {
+		time = time * 1000
+	  }
+	  date = new Date(time)
+	}
+	const formatObj = {
+	  y: date.getFullYear(),
+	  m: date.getMonth() + 1,
+	  d: date.getDate(),
+	  h: date.getHours(),
+	  i: date.getMinutes(),
+	  s: date.getSeconds(),
+	  a: date.getDay()
+	}
+	const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+	  let value = formatObj[key]
+	  // Note: getDay() returns 0 on Sunday
+	  if (key === 'a') { return ['日', '一', '二', '三', '四', '五', '六'][value ] }
+	  if (result.length > 0 && value < 10) {
+		value = '0' + value
+	  }
+	  return value || 0
+	})
+	return time_str
+}
+
 export default tool
