f5cee5d7d2aaaa1d6da25dfa36d2ad63f9018134
俞宝山 <1253070437@qq.com>
2024-11-04 15:35:08 +0800

    【更新】loading界面获得系统内设置的系统名称

--- snowy-admin-web/src/utils/loading.js
+++ snowy-admin-web/src/utils/loading.js
@@ -3,6 +3,8 @@
  * @method start 创建 loading
  * @method done 移除 loading
  */
+import sysConfig from '@/config/index'
+
 export const NextLoading = {
 	// 创建 loading
 	start: () => {
@@ -11,14 +13,16 @@ export const NextLoading = {
 		const body = document.body
 		const div = document.createElement('div')
 		div.setAttribute('class', 'admin-ui')
-		div.innerHTML = `
+		const sysName = sysConfig.SYS_BASE_CONFIG.SNOWY_SYS_NAME
+		const htmlBefore = `
 			  <div class="app-loading">
 				<div class="app-loading-logo">
 				  <img src="/img/logo.png"/>
 				</div>
 				<div><span class="dot dot-spin"><i></i><i></i><i></i><i></i></span></div>
-				<div class="app-loading-title">Snowy</div>
-			</div>`
+				<div class="app-loading-title"> `
+		const htmlAfter = ` </div></div>`
+		div.innerHTML = htmlBefore + sysName + htmlAfter
 		body.insertBefore(div, body.childNodes[0])
 		window.nextLoading = true
 	},
