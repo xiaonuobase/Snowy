d74422041b0504fc9437a3e0d4bcb5b17d10d903
xuyuxiang <xuyuxiang29@foxmail.com>
2026-07-06 23:43:44 +0800

    【底座】系统配置-推送配置中，钉钉、飞书、企业微信选项卡前加上对应图标

--- snowy-admin-web/src/views/dev/config/pushConfig/index.vue
+++ snowy-admin-web/src/views/dev/config/pushConfig/index.vue
@@ -1,12 +1,39 @@
 <template>
 	<a-tabs v-model:activeKey="activeKey" tab-position="left">
-		<a-tab-pane key="dingTalk" tab="钉钉">
+		<a-tab-pane key="dingTalk">
+			<template #tab>
+				<span>
+					<img
+						src="@/assets/images/authSource/dingtalk.png"
+						style="width: 20px; height: 20px; margin-right: 8px; vertical-align: middle"
+					/>
+					钉钉
+				</span>
+			</template>
 			<ding-talk-form />
 		</a-tab-pane>
-		<a-tab-pane key="feishu" tab="飞书">
+		<a-tab-pane key="feishu">
+			<template #tab>
+				<span>
+					<img
+						src="@/assets/images/authSource/feishu.png"
+						style="width: 20px; height: 20px; margin-right: 8px; vertical-align: middle"
+					/>
+					飞书
+				</span>
+			</template>
 			<feishu-form />
 		</a-tab-pane>
-		<a-tab-pane key="workWechat" tab="企业微信">
+		<a-tab-pane key="workWechat">
+			<template #tab>
+				<span>
+					<img
+						src="@/assets/images/authSource/workwechat.png"
+						style="width: 20px; height: 20px; margin-right: 8px; vertical-align: middle"
+					/>
+					企业微信
+				</span>
+			</template>
 			<work-wechat-form />
 		</a-tab-pane>
 	</a-tabs>
